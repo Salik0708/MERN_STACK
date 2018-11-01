c3788be043b3bea2af06508167470afb847ce0f4
Salik07 <salikhussain41@gmail.com>
2018-11-01 14:43:43 -0700

    Initial commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb://brad:brad123@ds147723.mlab.com:47723/shopping_list'
+  mongoURI: 'mongodb://brad:brad123@ds147723.mlab.com:47723/shopping_list' || 'mongodb://localhost:27017/ShoppingList'
 }
